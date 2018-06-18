759922385826ba30417a816152e3e5f0e70c7802
Jagroop Hothi <hothij@amazon.com>
2018-06-17 23:26:26 -0400

    Add a dependency to the pixel game engine library

--- cmd/chip/main.go
+++ cmd/chip/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-	"os"
-	"chip8/internal/emulator"
-	"fmt"
+	"github.com/faiface/pixel/pixelgl"
+	"github.com/faiface/pixel"
 )
 
+
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "Pixel Rocks!",
+		Bounds: pixel.R(0, 0, 1024, 768),
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+	for !win.Closed() {
+		win.Update()
+	}
+}
+
 func main() {
-	filePath := os.Args[1]
-	emu := emulator.New()
-	data := emu.Start(filePath)
-	fmt.Println(data)
+	//filePath := os.Args[1]
+	//emu := emulator.New()
+	//data := emu.Start(filePath)
+	//fmt.Println(data)
+	pixelgl.Run(run)
 }
